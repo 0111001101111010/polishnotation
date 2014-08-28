6428e7d84f8786a800bdb89ba08b76042fd20294
stanley zheng <dev.stanzheng@gmail.com>
2014-08-28 01:14:52 -0400

    adding tests

---
+++ test/test.js
@@ -0,0 +1,20 @@
+var assert = require('chai').assert;
+var p = require('../lib/lib');
+
+describe('Test Expressions', function() {
+var expression = ['3','4','-','5','+']; //4
+var expression2 = ['3','4','5','+','-']; // -6
+var expression3 = ['3','4','*','2',"/"]; // 6
+
+  it("The answer to ['3','4','-','5','+'] should be 4", function() {
+    assert.equal(p.postFix(expression),4,"it should be 4");
+  });
+
+  it("The answer to ['3','4','5','+','-'] should be -6", function() {
+    assert.equal(p.postFix(expression2), -6, "it should be -6");
+  });
+
+  it("The answer to ['3','4','*','2',"/"] should be 6", function() {
+    assert.equal(p.postFix(expression3), 6, "it should be 6");
+  });
+});
